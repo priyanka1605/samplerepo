00f42da639523a406501d31d736d1a7731ec2ab7
prwani <prwani@LIN25005184.corp.capgemini.com>
2021-08-16 16:03:40 +0530

    Commit all

---
+++ firstdemo/src/main/java/firstdemo/sample.java
@@ -0,0 +1,16 @@
+package firstdemo;
+
+public class sample {
+
+	public void display() {
+		int [] arr= {23,45,56,78};
+		for(int num:arr)
+			System.out.println(num);
+
+	}
+	public void print() {
+		System.out.println("Print all Array Numbers");
+		display();
+	}
+
+}
