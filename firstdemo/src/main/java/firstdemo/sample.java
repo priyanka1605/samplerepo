6ace778f8fc11a6dbe5a7baa375ba8358e046984
prwani <prwani@LIN25005184.corp.capgemini.com>
2021-08-17 11:42:41 +0530

    sample class display method added with sum.

--- firstdemo/src/main/java/firstdemo/sample.java
+++ firstdemo/src/main/java/firstdemo/sample.java
@@ -4,8 +4,12 @@ public class sample {
 
 	public void display() {
 		int [] arr= {23,45,56,78};
-		for(int num:arr)
-			System.out.println(num);
+		int sum=0;
+		for(int num:arr) {
+			sum+=num;
+		    System.out.println(num);
+		}
+		System.out.println("Summation:"+ sum);
 
 	}
 	public void print() {
