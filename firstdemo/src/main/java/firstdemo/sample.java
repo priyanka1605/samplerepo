74fc014528a32f77d79d68039cc7e526d390460c
prwani <prwani@LIN25005184.corp.capgemini.com>
2021-08-17 12:26:12 +0530

    Display method updated.

--- firstdemo/src/main/java/firstdemo/sample.java
+++ firstdemo/src/main/java/firstdemo/sample.java
@@ -5,10 +5,11 @@ public class sample {
 	public void display() {
 		int [] arr= {23,45,56,78};
 		int sum=0;
-		for(int num:arr) {
-			sum+=num;
-		    System.out.println(num);
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
+		    System.out.println(arr[i]);
 		}
+		System.out.println("array size:"+ arr.length);
 		System.out.println("Summation:"+ sum);
 
 	}
